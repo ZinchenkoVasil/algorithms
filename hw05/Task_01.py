15e37b12156b78f582fe3895c9abf3d1a249fe8e
zinchenkovasil <zinchenkovasil@mail.ru>
2018-11-14 17:12:56 +0300

    исправлено

--- hw05/Task_01.py
+++ hw05/Task_01.py
@@ -1,15 +1,13 @@
-companies = {}
+import collections
+companies = collections.defaultdict(list)
 n = int(input("Количество предприятий: "))
 s = 0
 for i in range(n):
     company_name = input(str(i+1) + "-е предприятие: ")
-    profit = [0, 0, 0, 0]
-    profit[0] = int(input("введите прибыль за 1 квартал: "))
-    profit[1] = int(input("введите прибыль за 2 квартал: "))
-    profit[2] = int(input("введите прибыль за 3 квартал: "))
-    profit[3] = int(input("введите прибыль за 4 квартал: "))
-    companies[company_name] = profit
-    s += sum(profit)
+    for j in range(1,5):
+        profit = int(input(f"введите прибыль за {j} квартал: "))
+        companies[company_name].append(profit)
+        s += profit
     avrg = s / n
 print("\nСредняя прибыль: %.0f. Предприятия с годовой прибылью выше среднего:" % avrg)
 for company_name in companies:
@@ -20,3 +18,4 @@ for company_name in companies:
     if sum(companies[company_name]) < avrg:
         print(company_name)
 
+#print(companies)
